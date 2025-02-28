bca13c9a759fd30cb857892579b903adb783cdc0
NutMegCode <madwyer@hotmail.com>
2025-02-28 16:33:14 +0800

    recipe screen ui

--- Paradigm Project/ViewController.swift
+++ Paradigm Project/ViewController.swift
@@ -8,14 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var methodLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "Spag Bol"
+        descriptionLabel.text = "A classic Italian dish"
+        methodLabel.text = "1. Boil water. \n2. Add pasta. \n3. Add sauce. \n4. Cook pasta."
+        
         // Do any additional setup after loading the view.
     }
-
-//Here I am editing the main from GitHub
-    
-    //Now I am editing the main from xCode
 }
 
