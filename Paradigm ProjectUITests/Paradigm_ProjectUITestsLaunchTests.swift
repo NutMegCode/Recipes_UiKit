71c13ad599c75d4696efa08210999462b89d062a
NutMegCode <madwyer@hotmail.com>
2025-02-28 14:14:07 +0800

    Initial Commit

---
+++ Paradigm ProjectUITests/Paradigm_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Paradigm_ProjectUITestsLaunchTests.swift
+//  Paradigm ProjectUITests
+//
+//  Created by Meg on 28/2/2025.
+//
+
+import XCTest
+
+final class Paradigm_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
